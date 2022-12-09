55d5de4883e938318cccc4980c82631d5b542db6
Sarah <vanwars@gmail.com>
2022-12-08 21:38:11 -0500

    Added list of prerequisites

--- main.js
+++ main.js
@@ -13,6 +13,59 @@ const textColors = {
     '': '#444'
 }
 
+function highlightPath (evt) {
+    const node = evt.target;
+    cy.edges().forEach(function (edge) {
+        edge.css('line-color', '#EEE');
+        edge.css('source-arrow-color', '#DDD');
+    });
+    cy.nodes().forEach(function (edge) {
+        edge.css('background-color', '#EEE');
+        edge.css('color', '#444');
+    });
+    node.style({
+        'background-color': '#6a8e7f',
+        'color': 'white'
+    });
+    var nodes = [];
+    cy.elements().dfs({
+        roots: `#${evt.target.id()}`,
+        visit: function(node, edge, u, i, depth) {
+            if (edge) {
+                var courseID = node.data().id.replace("CSCI", "CSCI ");
+                nodes.push(courseID);
+                edge.style({
+                    'line-color': '#6a8e7f',
+                    'source-arrow-color': '#6a8e7f'
+                })
+                node.style({
+                    'background-color': '#6a8e7f',
+                    'color': 'white'
+                })
+            }
+        },
+        directed: true
+    });
+    const data = node.data();
+    data.prerequisites = `<ul><li>${nodes.join("</li><li>")}</li></ul>`;
+    document.querySelector('.course-info').innerHTML = `
+        <h3>${data.id.replace("CSCI", "CSCI ")}: ${data.title}</h3>
+        <p>${data.description ? data.description : 'Some description of the course...'}</p>
+        ${data.systems || data.info || data.minor ? '<h4>Required For:</h4>' : ''}
+        ${data.systems ? '<span class="systems">Computer Systems</span>' : ''}
+        ${data.info ? '<span class="info">Information Systems </span>' : ''}
+        ${data.minor ? '<span class="minor">CS Minor</span>' : ''}
+        <h3>Prerequisites</h3>
+        <p>${data.prerequisites}</p>
+    `;
+}
+
+// function showCourseInfo (evt) {
+//     const data = evt.target.data();
+
+    
+// }
+
 
 function getCoursesForSpecialization(elements, key) {
     if (!key) {
@@ -54,8 +107,8 @@ function draw() {
 
         ready: function () {
             this.nodes().forEach(function (node) {
-                const dependencies = node.data().dependencies;
-                console.log(dependencies + 2, Math.log2(dependencies + 2) * 100)
+                // const dependencies = node.data().dependencies;
+                // console.log(dependencies + 2, Math.log2(dependencies + 2) * 100)
                 //let size = Math.log2(dependencies + 2) * 12 + 10; // + dependencies * 30;
                 let size = 35;
                 node.css("width", size);
@@ -136,60 +189,7 @@ function draw() {
         elements: getCoursesForSpecialization(elements, key)
     });
 
-
-    cy.on('click', 'node', (e) => {
-        const data = e.target.data();
-
-        document.querySelector('.course-info').innerHTML = `
-            <h3>${data.title}</h3>
-            <p>${data.description ? data.description : 'Some description of the course...'}</p>
-            ${data.systems || data.info || data.minor ? '<h4>Required For:</h4>' : ''}
-            ${data.systems ? '<span class="systems">Computer Systems</span>' : ''}
-            ${data.info ? '<span class="info">Information Systems </span>' : ''}
-            ${data.minor ? '<span class="minor">CS Minor</span>' : ''}
-        `
-    });
-
-    cy.bind('click', 'node', function(evt) {
-        cy.edges().forEach(function (edge) {
-            console.log(edge);
-            edge.css('line-color', '#EEE');
-            edge.css('source-arrow-color', '#DDD');
-        });
-        cy.nodes().forEach(function (edge) {
-            console.log(edge);
-            edge.css('background-color', '#EEE');
-            edge.css('color', '#444');
-        });
-        console.log(`#${evt.target.id()}`);
-        evt.target.style({
-            'background-color': '#6a8e7f',
-            'color': 'white'
-        });
-        var edges = cy.collection();
-        var dfs = cy.elements().dfs({
-            roots: `#${evt.target.id()}`,
-            visit: function(v, e, u, i, depth) {
-                // console.log('visit ' + v.id());
-                if (e) {
-                    edges = edges.add(e);
-                    e.style({
-                        'line-color': '#6a8e7f',
-                        'source-arrow-color': '#6a8e7f'
-                    })
-                    v.style({
-                        'background-color': '#6a8e7f',
-                        'color': 'white'
-                    })
-                }
-            },
-            directed: true
-        });
-      
-        // console.log(dfs);
-        var path = dfs.path;
-        path.select();
-    });
+    cy.bind('click', 'node', highlightPath);
 }
 
 
